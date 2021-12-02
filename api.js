ceacd6911c362fe443a9acbdd9f3ec69a2b5bb6f
wii3official <91664755+wii3official@users.noreply.github.com>
2021-12-02 21:49:21 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressGraphQL = require("express-graphql");
+const serverless = require("serverless-http");
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'Wii3root')));
 
-app.get('/', function (req, res) {
+app.set('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'Wii3root', 'index.html'));
   console.log("sent somethin");
 });
 
-app.listen(9000);
+
 module.exports.handler = serverless(app);
+
